4caeb8a0969fb6ae3196a8e72b4848c0f18df0ce
PedroUtida <61791024+PedroUtida@users.noreply.github.com>
2021-11-19 18:02:16 -0300

    Delete src directory

--- src/br/com/fiap/tds/dao/AlimentoDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.tds.dao;
-
-import br.com.fiap.entity.Alimento;
-
-public interface AlimentoDao extends GenericDao<Alimento, Integer>{
-
-}
--- src/br/com/fiap/tds/dao/UsuarioDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.tds.dao;
-
-import br.com.fiap.entity.Usuario;
-
-public interface UsuarioDao extends GenericDao<Usuario, Integer>{
-
-}
